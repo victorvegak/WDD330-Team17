c6762abcbfdf592ade601d61ccc308a4da11cbfa
myJeeyoungM78 <kim22008@byui.edu>
2025-11-08 01:16:11 -0700

    W02_IndiA_Step4

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,15 @@
+export default class ProductList {
+  constructor(category, dataSource, listElement) {
+    // You passed in this information to make the class as reusable as possible.
+    // Being able to define these things when you use the class will make it very flexible
+    this.category = category;
+    this.dataSource = dataSource;
+    this.listElement = listElement;
+  }
+
+  async init() {
+    // the dataSource will return a Promise...so you can use await to resolve it.
+    const list = await this.dataSource.getData();
+    // next, render the list – ** future **
+  }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
 
 const dataSource = new ProductData("tents");
 
