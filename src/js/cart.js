439e3348415f42a68268edeb7612121e8b8f2462
LarrySon786 <richardsbrandon3@gmail.com>
2025-11-20 23:10:54 -0500

    team activity week4

--- src/js/cart.js
+++ src/js/cart.js
@@ -26,8 +26,26 @@ function cartItemTemplate(item) {
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
-
+  
   return newItem;
+}
+
+function cartSummaryInfo(item) {
+  return `<li class="cart-card divider">
+  <a href="#" class="cart-card__image">
+    <img
+      src="${item.Image}"
+      alt="${item.Name}"
+    />
+  </a>
+  <a href="#">
+    <h2 class="card__name">${item.Name}</h2>
+  </a>
+  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__price">$${item.FinalPrice}</p>
+</li>`;
+
 }
 
 loadHeaderFooter();
