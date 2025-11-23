c732ff2ee0266d71016270327a37f5b6eba9b073
myJeeyoungM78 <kim22008@byui.edu>
2025-11-23 00:16:02 -0700

    jm--team4

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,4 +1,19 @@
 import { loadHeaderFooter } from "./utils.mjs";
-
+import { CheckoutProcess } from "./checkout-process.mjs";
 
 loadHeaderFooter();
+
+const order = new CheckoutProcess("so-cart", ".checkout-summary");
+order.init();
+
+// Add event listeners to fire calculateOrderTotal when the user changes the zip code
+document
+  .querySelector("#zip")
+  .addEventListener("blur", order.calculateOrderTotal.bind(order));
+
+// listening for click on the button
+document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  order.checkout();
+});
